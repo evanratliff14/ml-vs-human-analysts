d9d329c9e9e340179661309cb543e5a10698b0ed
evanratliff14 <evanratliff14@gmail.com>
2025-01-31 17:24:24 -0500

    Commit before uninstalling vscode

--- rb_gb.py
+++ rb_gb.py
@@ -7,11 +7,12 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 ##BEFORE I RUN FOR THE FIRST TIME: INSTALL JUPYTER LAB AND PYTHON EXTENSIONS + OPEN IN JUPYTER LAB
 #MIGHT HAVE TO RENAME USER
+# COMMIT EVERY DAY
 
 class rb_gb:
-    def __init__():
-        self.__train_years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 1010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-        self.__test_years = [2021, 2022]
+    def __init__(self):
+        self._train_years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 1010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+        self._test_years = [2021, 2022]
         with open('fantasy_data.pkl', 'rb') as file:
             fantasy_data = pickle.load(file)
 
@@ -24,14 +25,11 @@ class rb_gb:
             initial_strategy='median', random_state=42, 
             add_indicator=False)
 
-        self.__rb_test = rb_data.loc[rb_data['season'].isin[self.__train_years]]
-        self.__rb_train = rb_data.loc[rb_data['season'].isin[self.__test_years]]
+        self._rb_test = rb_data.loc[rb_data['season'].isin[self.__train_years]]
+        self._rb_train = rb_data.loc[rb_data['season'].isin[self.__test_years]]
 
-        self.__features = [
-            'completions', 'attempts', 'passing_yards', 
-            'passing_tds', 'interceptions', 'sacks', 'sack_yards', 'sack_fumbles', 'sack_fumbles_lost', 
-            'passing_air_yards', 'passing_yards_after_catch', 'passing_epa', 'passing_2pt_conversions', 
-            'pacr', 'dakota', 'carries', 'rushing_yards', 'rushing_tds', 'rushing_fumbles', 
+        self._features = [
+            'pacr', 'carries', 'rushing_yards', 'rushing_tds', 'rushing_fumbles', 
             'rushing_fumbles_lost', 'rushing_epa', 'rushing_2pt_conversions', 'receptions', 'targets', 
             'receiving_yards', 'receiving_tds', 'receiving_fumbles', 'receiving_fumbles_lost', 
             'receiving_air_yards', 'receiving_yards_after_catch', 'receiving_first_downs', 'receiving_epa', 
@@ -40,13 +38,42 @@ class rb_gb:
             'wopr_y', 'ry_sh', 'rtd_sh', 'rfd_sh', 'rtdfd_sh', 'w8dom', 'player_name', 'position', 
             'fantasy_points_half_ppr', 'ir_games', 'out_games', 'avg_separation', 'avg_yac_above_expectation', 
             'catch_percentage', 'rush_yards_over_expected_per_att', 'rush_pct_over_expected', 
-            'completion_percentage_above_expectation', 'pass_tds/game', 'pass_air_yards/game', 'carries/game
+            'completion_percentage_above_expectation', 'pass_tds/game', 'pass_air_yards/game', 'carries/game', 
+            'yards/carry', 'rushing_tds/game', 'receiving_tds/game', 'turnovers/game', 'adot', 'targets_game', 
+            'team', 'depth_chart_position', 'height', 'weight', 'years_exp', 'age', 'depth_team', 'win_total_adj_line', 
+            'gsis_id', 'college', 'pick', 'draft_value', 'forty', 'bench', 'vertical', 'broad_jump', 'cone', 
+            'shuttle', 'next_season', 'next_season_future', 'fantasy_points_future', 'fantasy_points_ppr_future', 
+            'fantasy_points_half_ppr_future', 'games_future', 'team_future', 'future_ppr/game', 'future_half_ppr/game', 
+            'ppr/game', 'half_ppr/game', 'receiving_epa_team_tes', 'rushing_epa_team_qbs', 'dakota_team_qbs', 
+            'passing_epa_team_qbs', 'receiving_epa_team_wrs', 'receiving_epa_team_rbs', 'rushing_epa_team_rbs'
             ]
 
-        
-    def __train_model():
-        rb_gb_model = GradientBoostingRegressor()
 
+        
+    def train_model(self):
+        # what is friedman mse and why is it better?
+        # min samples split --> less overfitting?
+        # random state on?
+        # n estimators is number of trees in the ensemble
+        # use max leaf nodes instead of max depth??
+        # validation_fraction is 0.0 because we are using our own cross validation methods
+        # can use impurity decreate, max depth, or max leaf nodes. impurity decrease measures the MSE loss of a node
+        # can use a combo of size and impurity based limits
+        self.model = GradientBoostingRegressor（loss='squared_error', learning_rate=0.1, n_estimators=100,
+            criterion='friedman_mse', min_samples_split=None, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
+            max_depth=None，min_impurity_decrease=64.0, init=None, random_state=42, max_features=None, alpha=0.9, 
+            verbose=0, max_leaf_nodes=15, warm_start=False, validation_fraction=0.0, n_iter_no_change=None,
+            tol=0.0001, ccp_alpha=0.0)[self._rb_train[self._features], self._rb_train['fantasy_points_half_ppr_future']]
+
+
+    def test(self):
+        self._rb_test['results'] = self.model(self._rb_test[features], self._rb_test['fantasy_points_half_ppr_future'])
+
+    def corr_matrix(self):
+
+    def shap(self):
+
+    def permutation_importance
 
         
 
@@ -55,6 +82,7 @@ class rb_gb:
 # set up corr matrix
 # todo: set on only top k
 # set up permutation importance
+#calculate other rbs_epa
 # todo: set up shap values
 
 ## output:
