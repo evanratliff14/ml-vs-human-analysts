51530b9db08ef1bb9da08dafbcaf728459e12a43
evanratliff14 <evanratliff14@gmail.com>
2025-02-01 16:30:29 -0500

    Commit

--- rb_gb.py
+++ rb_gb.py
@@ -5,14 +5,18 @@ from sklearn.impute import IterativeImputer
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
+# feature selection
+from sklearn.feature_selection import SequentialFeatureSelector
+
+
 ##BEFORE I RUN FOR THE FIRST TIME: INSTALL JUPYTER LAB AND PYTHON EXTENSIONS + OPEN IN JUPYTER LAB
 #MIGHT HAVE TO RENAME USER
 # COMMIT EVERY DAY
 
 class rb_gb:
     def __init__(self):
-        self._train_years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 1010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-        self._test_years = [2021, 2022]
+        self.__train_years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 1010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+        self.__test_years = [2021, 2022]
         with open('fantasy_data.pkl', 'rb') as file:
             fantasy_data = pickle.load(file)
 
@@ -25,32 +29,32 @@ class rb_gb:
             initial_strategy='median', random_state=42, 
             add_indicator=False)
 
-        self._rb_test = rb_data.loc[rb_data['season'].isin[self.__train_years]]
-        self._rb_train = rb_data.loc[rb_data['season'].isin[self.__test_years]]
+        self.__rb_test = rb_data.loc[rb_data['season'].isin[self.__train_years]]
+        self.__rb_train = rb_data.loc[rb_data['season'].isin[self.__test_years]]
+        self.__rb_data = rb_data
 
-        self._features = [
+        self.__features = [
             'pacr', 'carries', 'rushing_yards', 'rushing_tds', 'rushing_fumbles', 
             'rushing_fumbles_lost', 'rushing_epa', 'rushing_2pt_conversions', 'receptions', 'targets', 
             'receiving_yards', 'receiving_tds', 'receiving_fumbles', 'receiving_fumbles_lost', 
             'receiving_air_yards', 'receiving_yards_after_catch', 'receiving_first_downs', 'receiving_epa', 
             'receiving_2pt_conversions', 'racr', 'target_share', 'air_yards_share', 'wopr_x', 
-            'special_teams_tds', 'fantasy_points', 'fantasy_points_ppr', 'games', 'tgt_sh', 'ay_sh', 
-            'wopr_y', 'ry_sh', 'rtd_sh', 'rfd_sh', 'rtdfd_sh', 'w8dom', 'player_name', 'position', 
+            'games', 'tgt_sh', 'ay_sh', 
+            'wopr_y', 'ry_sh', 'rtd_sh', 'rfd_sh', 'rtdfd_sh', 'w8dom', 
             'fantasy_points_half_ppr', 'ir_games', 'out_games', 'avg_separation', 'avg_yac_above_expectation', 
             'catch_percentage', 'rush_yards_over_expected_per_att', 'rush_pct_over_expected', 
-            'completion_percentage_above_expectation', 'pass_tds/game', 'pass_air_yards/game', 'carries/game', 
+            'carries/game', 
             'yards/carry', 'rushing_tds/game', 'receiving_tds/game', 'turnovers/game', 'adot', 'targets_game', 
             'team', 'depth_chart_position', 'height', 'weight', 'years_exp', 'age', 'depth_team', 'win_total_adj_line', 
             'gsis_id', 'college', 'pick', 'draft_value', 'forty', 'bench', 'vertical', 'broad_jump', 'cone', 
-            'shuttle', 'next_season', 'next_season_future', 'fantasy_points_future', 'fantasy_points_ppr_future', 
+            'shuttle', 'fantasy_points_future', 'fantasy_points_ppr_future', 
             'fantasy_points_half_ppr_future', 'games_future', 'team_future', 'future_ppr/game', 'future_half_ppr/game', 
-            'ppr/game', 'half_ppr/game', 'receiving_epa_team_tes', 'rushing_epa_team_qbs', 'dakota_team_qbs', 
-            'passing_epa_team_qbs', 'receiving_epa_team_wrs', 'receiving_epa_team_rbs', 'rushing_epa_team_rbs'
+            'ppr/game', 'half_ppr/game', 'receiving_epa_team_tes', 'rushing_epa_team_qbs',  'receiving_epa_team_wrs', 'receiving_epa_team_rbs', 'rushing_epa_team_rbs'
             ]
-
-
+        self.__set_features()
+        self.__train_model()
         
-    def train_model(self):
+    def __train_model(self):
         # what is friedman mse and why is it better?
         # min samples split --> less overfitting?
         # random state on?
@@ -59,7 +63,7 @@ class rb_gb:
         # validation_fraction is 0.0 because we are using our own cross validation methods
         # can use impurity decreate, max depth, or max leaf nodes. impurity decrease measures the MSE loss of a node
         # can use a combo of size and impurity based limits
-        self.model = GradientBoostingRegressor（loss='squared_error', learning_rate=0.1, n_estimators=100,
+        self.model = GradientBoostingRegressor(loss='squared_error', learning_rate=0.1, n_estimators=100,
             criterion='friedman_mse', min_samples_split=None, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
             max_depth=None，min_impurity_decrease=64.0, init=None, random_state=42, max_features=None, alpha=0.9, 
             verbose=0, max_leaf_nodes=15, warm_start=False, validation_fraction=0.0, n_iter_no_change=None,
@@ -69,11 +73,21 @@ class rb_gb:
     def test(self):
         self._rb_test['results'] = self.model(self._rb_test[features], self._rb_test['fantasy_points_half_ppr_future'])
 
-    def corr_matrix(self):
+    def __set_features(self):
+        selector = SequentialFeatureSelector(self.model, *, n_features_to_select='auto', tol=None, direction='forward', scoring="mean_absolute_error", cv=5, n_jobs=-1)[rb_train]
+        indices_selected_arr = selector.getSupport(indices = True).toList()
+        features = []
+        for i in indices_selected_arr:
+            features.append(self.__features[i])
+        self.__features = features
 
-    def shap(self):
 
-    def permutation_importance
+
+
+
+    # def shap(self):
+
+    # def permutation_importance
 
         
 
