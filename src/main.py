62e0d0115e5d87e7231623eac68360c721c16f25
Evan Ratliff <evanratliff@Evans-MacBook-Air.local>
2025-10-26 22:49:22 -0400

    add wide receiver model

--- src/main.py
+++ src/main.py
@@ -13,9 +13,9 @@ class __main__:
             fdf.players_stats.to_parquet('data.parquet', index=False)
         
         # self.rb_seasonal = Seasonal(points_type='ppr', position = 'RB', type = 'xgb')
-        self.qb_seasonal = Seasonal(points_type='ppr', position = 'QB', type = 'xgb')
+        # self.qb_seasonal = Seasonal(points_type='ppr', position = 'QB', type = 'xgb')
         # self.te_seasonal = Seasonal(points_type='ppr', position = 'TE', type = 'xgb')
-        # self.wr_seasonal = Seasonal(points_type='ppr', position = 'WR', type = 'xgb')
+        self.wr_seasonal = Seasonal(points_type='ppr', position = 'WR', type = 'xgb')
 
 
     def run(self):
@@ -29,14 +29,14 @@ class __main__:
         # rb_seasonal.cross_validate()
 
         
-        qb_seasonal = self.qb_seasonal
-        qb_seasonal.corr()
+        # qb_seasonal = self.qb_seasonal
+        # qb_seasonal.corr()
 
-        # only outputting standard/game right now
-        # rb_seasonal.set_features()
-        qb_seasonal.train_model(qb_seasonal.model)
-        qb_seasonal.test_model()
-        qb_seasonal.cross_validate()
+        # # only outputting standard/game right now
+        # # rb_seasonal.set_features()
+        # qb_seasonal.train_model(qb_seasonal.model)
+        # qb_seasonal.test_model()
+        # qb_seasonal.cross_validate()
 
         # te_seasonal = self.te_seasonal
         # te_seasonal.corr()
@@ -47,21 +47,21 @@ class __main__:
         # te_seasonal.test_model()
         # te_seasonal.cross_validate()
 
-        # wr_seasonal = self.wr_seasonal
-        # wr_seasonal.corr()
+        wr_seasonal = self.wr_seasonal
+        wr_seasonal.corr()
 
-        # # only outputting standard/game right now
-        # # wr_seasonal.set_features()
-        # wr_seasonal.train_model(wr_seasonal.model)
-        # wr_seasonal.test_model()
-        # wr_seasonal.cross_validate()
+        # only outputting standard/game right now
+        # wr_seasonal.set_features()
+        wr_seasonal.train_model(wr_seasonal.model)
+        wr_seasonal.test_model()
+        wr_seasonal.cross_validate()
 
 
 
         # print(rb_seasonal)
         # print(te_seasonal)
-        # print(wr_seasonal)
-        print(qb_seasonal)
+        print(wr_seasonal)
+        # print(qb_seasonal)
 
 
 if __name__ == '__main__':
