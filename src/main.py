9e70c7ea65a9680c272e563fe8ab44220a8653ce
Evan Ratliff <evanratliff@Evans-MacBook-Air.local>
2025-10-26 22:52:35 -0400

    add te model

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ class __main__:
         
         # self.rb_seasonal = Seasonal(points_type='ppr', position = 'RB', type = 'xgb')
         # self.qb_seasonal = Seasonal(points_type='ppr', position = 'QB', type = 'xgb')
-        # self.te_seasonal = Seasonal(points_type='ppr', position = 'TE', type = 'xgb')
-        self.wr_seasonal = Seasonal(points_type='ppr', position = 'WR', type = 'xgb')
+        self.te_seasonal = Seasonal(points_type='ppr', position = 'TE', type = 'xgb')
+        # self.wr_seasonal = Seasonal(points_type='ppr', position = 'WR', type = 'xgb')
 
 
     def run(self):
@@ -38,29 +38,29 @@ class __main__:
         # qb_seasonal.test_model()
         # qb_seasonal.cross_validate()
 
-        # te_seasonal = self.te_seasonal
-        # te_seasonal.corr()
-
-        # # only outputting standard/game right now
-        # # te_seasonal.set_features()
-        # te_seasonal.train_model(te_seasonal.model)
-        # te_seasonal.test_model()
-        # te_seasonal.cross_validate()
-
-        wr_seasonal = self.wr_seasonal
-        wr_seasonal.corr()
+        te_seasonal = self.te_seasonal
+        te_seasonal.corr()
 
         # only outputting standard/game right now
-        # wr_seasonal.set_features()
-        wr_seasonal.train_model(wr_seasonal.model)
-        wr_seasonal.test_model()
-        wr_seasonal.cross_validate()
+        # te_seasonal.set_features()
+        te_seasonal.train_model(te_seasonal.model)
+        te_seasonal.test_model()
+        te_seasonal.cross_validate()
+
+        # wr_seasonal = self.wr_seasonal
+        # wr_seasonal.corr()
+
+        # # only outputting standard/game right now
+        # # wr_seasonal.set_features()
+        # wr_seasonal.train_model(wr_seasonal.model)
+        # wr_seasonal.test_model()
+        # wr_seasonal.cross_validate()
 
 
 
         # print(rb_seasonal)
-        # print(te_seasonal)
-        print(wr_seasonal)
+        print(te_seasonal)
+        # print(wr_seasonal)
         # print(qb_seasonal)
 
 
